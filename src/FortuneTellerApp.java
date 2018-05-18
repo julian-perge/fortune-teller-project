ea6829ad4b74d688300de2b88389ccb027af4187
weapon-x <julianperge@gmail.com>
2018-05-18 14:07:04 -0400

    Finished adding and tweaking rest of code. Also added basic try/catch method for NumberFormatException

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -7,55 +7,68 @@ public class FortuneTellerApp
 	{
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
+
 		int yearsToRetire = 0;
 		double bankBalance = 0;
 		String vacationLocation = "";
 		String modeOfTransportation = "";
 		String quittingApp = "Quit";
-				
+
 		// first name
-		while (input.nextLine() != quittingApp.toLowerCase())
+		System.out.println("Please enter your first name.");
+		
+		try
 		{
-			System.out.println("Please enter your first name.");
 			String firstName = input.nextLine();
-			if (firstName.isEmpty())
+			if (firstName.equalsIgnoreCase(quittingApp))
 			{
-				System.out.println("This field cannot be blank. Please try again.");
-				firstName = input.nextLine();
+				System.out.println("Nobody likes a quitter...");
+				System.exit(0);
 			}
+
 			// last name
 			System.out.println("Please enter your last name (sur name).");
 			String lastName = input.nextLine();
-			if (lastName.isEmpty())
+			if (lastName.equalsIgnoreCase(quittingApp))
 			{
-				System.out.println("This field cannot be blank. Please try again.");
-				lastName = input.nextLine();
+				System.out.println("Nobody likes a quitter...");
+				System.exit(0);
 			}
-			
+
 			// age
 			System.out.println("How old are you?");
-			int userAge = input.nextInt();
-			if (userAge <= 1 || userAge > 90)
+			String userAge = input.nextLine();
+			int parsedUserAge = Integer.parseInt(userAge);
+
+			if (userAge.equalsIgnoreCase(quittingApp))
 			{
-				System.out.println("How are you alive?!");
+				System.out.println("Nobody likes a quitter...");
+				System.exit(0);
 			}
-			else if ((userAge % 2) == 0)
+			else
 			{
-				yearsToRetire = 40;
+				if (parsedUserAge <= 1 || parsedUserAge > 90)
+				{
+					System.out.println("How are you alive?!");
+				}
+				else if ((parsedUserAge % 2) == 0)
+				{
+					yearsToRetire = 40;
+				}
+				else
+				{
+					yearsToRetire = 33;
+				}
 			}
-			
-			yearsToRetire = 33;
-			
+
 			// birth month && bank account
 			System.out.println("What is your birth month? Enter a number 1-12, January being 1.");
 			String birthMonth = input.nextLine();
 			int intBirthMonth = Integer.parseInt(birthMonth);
-			
-			if (intBirthMonth > 12 || intBirthMonth <= 0)
+
+			if (intBirthMonth > 12 || intBirthMonth <= 0 || birthMonth.equals(""))
 			{
-				System.out.println("Please enter a valid birth month.\n"
-						+ "Only numbers 1 through 12 are applicable.");
+				System.out.println("Birth month is not valid.\n" + "Only numbers 1 through 12 are applicable.");
 			}
 			else
 			{
@@ -85,50 +98,54 @@ public class FortuneTellerApp
 						break;
 				}
 			}
-			
+
 			// favorite color
 			System.out.println("What is your favorite ROYGBIV color?");
 			String favoriteColor = input.nextLine();
+
+			while (favoriteColor.equalsIgnoreCase("Help"))
+			{
+				System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
+				favoriteColor = input.nextLine();
+			}
+
 			if (favoriteColor.isEmpty())
 			{
 				System.out.println("No favorite color :( ");
 				favoriteColor = "N/A";
-			}
-			else if (favoriteColor.equalsIgnoreCase("help"))
-			{
-				System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
 			}
 			else
 			{
 				switch (favoriteColor.toLowerCase())
 				{
 					case "red":
-						favoriteColor = "Red";
+						modeOfTransportation = "Ferrari LaFerrari";
 						break;
 					case "orange":
-						favoriteColor = "Orange";
+						modeOfTransportation = "Dodge Viper SRT";
 						break;
 					case "yellow":
-						favoriteColor = "Yellow";
+						modeOfTransportation = "McLaren P1 GTR";
 						break;
 					case "green":
-						favoriteColor = "Green";
+						modeOfTransportation = "Lamborghini Murciélago";
 						break;
 					case "blue":
-						favoriteColor = "Blue";
+						modeOfTransportation = "Ferrari F60 America";
 						break;
 					case "indigo":
-						favoriteColor = "Indigo";
+						modeOfTransportation = "Noble M600";
 						break;
 					case "violet":
-						favoriteColor = "Violet";
+						modeOfTransportation = "Lamborghini Aventador Galaxy";
 						break;
 					default:
 						System.out.println("You did not choose a color in the ROYGBIV");
+						modeOfTransportation = "Flying Saucers";
 						break;
 				}
 			}
-			
+
 			// siblings
 			System.out.println("How many siblings do you have?");
 			int numOfSiblings = input.nextInt();
@@ -144,29 +161,32 @@ public class FortuneTellerApp
 					vacationLocation = "Hong Kong, China";
 					break;
 				case 3:
-					if (numOfSiblings > 3)
-					{
-						vacationLocation = "Sicily, Italy";
-					}
-					
 					vacationLocation = "Scottsdale, Arizona";
 					break;
 
 				default:
-					System.out.println("I didn't know you could have less than 1 sibling...");
+					if (numOfSiblings > 3)
+					{
+						vacationLocation = "Sicily, Italy";
+						break;
+					}
+					System.out.println("I didn't know you could have less than 0 sibling...");
 					vacationLocation = "Chernobyl, Ukraine";
 					break;
 			}
-			
-			
-			System.out.println(firstName + " " + lastName + " will retire in " + yearsToRetire + " with " + bankBalance + " in the bank, "
-					+ "a vacation home in " + vacationLocation + ", and travel by " + modeOfTransportation + ".");
-			
-			input.close();
-			break;
+
+			System.out.println(firstName + " " + lastName + " will retire in " + yearsToRetire + " years with $"
+					+ bankBalance + " in the bank, " + "a vacation home in " + vacationLocation + ", and travel by "
+					+ modeOfTransportation + ".");
 		}
-		
-		System.out.println("Nobody likes a quitter...");
+		catch (NumberFormatException e)
+		{
+			System.out.println(e + ", is not a valid input");
+			System.exit(0);
+		}
+
+		input.close();
+
 		System.exit(0);
 	}
 
